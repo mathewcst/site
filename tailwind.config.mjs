6f46113e6bd65d0d57dd05133139fa3438400841
Matheus Costa <mathfernandesz@gmail.com>
2023-11-10 07:55:38 -0300

    add meta tags

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "m-purple": "#501068",
+        "m-purple-dark": "#450c57",
+        "m-teal": "#7bffc9",
+        "m-blue": "#5a7eef",
+      },
+    },
+  },
+  plugins: [],
+};
