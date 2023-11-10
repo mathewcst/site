e04c88e1f0392d5ac2c76011ab4136e45c21add8
Matheus Costa <mathfernandesz@gmail.com>
2023-11-10 06:47:04 -0300

    Move to Astro

--- svelte.config.js
+++ svelte.config.js
@@ -1,49 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@astrojs/svelte';
 
-import { escapeSvelte, mdsvex } from 'mdsvex';
-
-// Code Hightlighting
-import shiki from 'shiki';
-
-// Markdown Plugins
-import rehypeSlug from 'rehype-slug';
-import remarkToc from 'remark-toc';
-import remarkUnwrapImages from 'remark-unwrap-images';
-
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-	extensions: ['.md'],
-	layout: {
-		_: './src/lib/mdsvex/layout.svelte'
-	},
-	highlight: {
-		highlighter: async (code, lang = 'text') => {
-			const highlighter = await shiki.getHighlighter({
-				theme: 'dracula'
-			});
-
-			let html = escapeSvelte(highlighter.codeToHtml(code, { lang }));
-			html = html.replace(/<pre/g, `<pre data-code="${code}"`);
-
-			return `{@html \`${html}\`}`;
-		}
-	},
-	remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true, ordered: true }]],
-	rehypePlugins: [rehypeSlug]
+export default {
+	preprocess: vitePreprocess(),
 };
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$: './src'
-		}
-	}
-};
-
-export default config;
